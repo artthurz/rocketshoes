4648bb3e3fdcaddcc9f06ed242a58abc47555054
Arthur Hauenstein <arthur_hauenstein@fruki.com.br>
2019-08-03 03:16:23 -0300

    finished

--- src/store/modules/cart/actions.js
+++ src/store/modules/cart/actions.js
@@ -1,27 +1,35 @@
 export function addToCartRequest(id) {
-  return{
+  return {
     type: '@cart/ADD_REQUEST',
-      id,
+    id,
   };
 }
 
 export function addToCartSuccess(product) {
-  return{
+  return {
     type: '@cart/ADD_SUCCESS',
     product,
   };
 }
 
 export function removeFromCart(id) {
-  return{
+  return {
     type: '@cart/REMOVE',
     id,
   };
 }
 
-export function updateAmount(id, amount) {
-  return{
-    type: '@cart/UPDATE_AMOUNT',
+export function updateAmountRequest(id, amount) {
+  return {
+    type: '@cart/UPDATE_AMOUNT_REQUEST',
+    id,
+    amount,
+  };
+}
+
+export function updateAmountSuccess(id, amount) {
+  return {
+    type: '@cart/UPDATE_AMOUNT_SUCCESS',
     id,
     amount,
   };
